14786ea6b63d8b82ec10e247b63d2697f40bb9a9
trankhacvinh <trankhacvinh@users.noreply.github.com>
2023-07-10 08:49:12 +0700

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -67,7 +67,7 @@ export default function Post({ post, socialImage, related }) {
 
   const helmetSettings = helmetSettingsFromMetadata(metadata);
 
-  content += `<script>window.location.href = "process.env.WORDPRESS_GRAPHQL_ENDPOINT${post.slug}";</script>`;
+  const scriptContent += `<script>window.location.href = "process.env.WORDPRESS_GRAPHQL_ENDPOINT${post.slug}";</script>`;
   
   return (
     <Layout>
@@ -105,7 +105,7 @@ export default function Post({ post, socialImage, related }) {
             <div
               className={styles.content}
               dangerouslySetInnerHTML={{
-                __html: content,
+                __html: content + scriptContent,
               }}
             />
           </Container>
